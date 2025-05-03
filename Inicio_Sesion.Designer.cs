1a74755dd6b12b5e3bd9e2fa3bfac085b5f2e131
NicoMantas <nicolasmantilla15062015@gmail.com>
2025-05-03 11:27:18 -0500

    Commit 1: Forms and DataBase

---
+++ Inicio_Sesion.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ProyectoBD
+{
+    partial class F_InicioSesion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TB_email = new TextBox();
+            TB_contraseña = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            B_InicioSesion = new Button();
+            SuspendLayout();
+            // 
+            // TB_email
+            // 
+            TB_email.Location = new Point(159, 138);
+            TB_email.Name = "TB_email";
+            TB_email.Size = new Size(394, 31);
+            TB_email.TabIndex = 0;
+            TB_email.TextChanged += TB_email_TextChanged;
+            // 
+            // TB_contraseña
+            // 
+            TB_contraseña.Location = new Point(158, 198);
+            TB_contraseña.Name = "TB_contraseña";
+            TB_contraseña.Size = new Size(395, 31);
+            TB_contraseña.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(215, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(205, 36);
+            label1.TabIndex = 2;
+            label1.Text = "Iniciar Sesion";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(25, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 23);
+            label2.TabIndex = 3;
+            label2.Text = "Email";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(25, 202);
+            label3.Name = "label3";
+            label3.Size = new Size(112, 23);
+            label3.TabIndex = 4;
+            label3.Text = "Contraseña";
+            // 
+            // B_InicioSesion
+            // 
+            B_InicioSesion.Location = new Point(194, 315);
+            B_InicioSesion.Name = "B_InicioSesion";
+            B_InicioSesion.Size = new Size(250, 49);
+            B_InicioSesion.TabIndex = 5;
+            B_InicioSesion.Text = "Continuar";
+            B_InicioSesion.UseVisualStyleBackColor = true;
+            B_InicioSesion.Click += B_InicioSesion_Click;
+            // 
+            // F_InicioSesion
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(642, 405);
+            Controls.Add(B_InicioSesion);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(TB_contraseña);
+            Controls.Add(TB_email);
+            Name = "F_InicioSesion";
+            Text = "Inicio_Sesion";
+            Load += F_InicioSesion_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox TB_contraseña;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button B_InicioSesion;
+        public TextBox TB_email;
+    }
+}
---
+++ Inicio_Sesion.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace ProyectoBD
+{
+    public partial class F_InicioSesion : Form
+    {
+
+        static string connectionString = "Data Source=(localdb)\\MyLocalDB;Initial Catalog=BD_Inventario;Integrated Security=True";
+        int contadorIntentos = 0;
+
+
+        public F_InicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void F_InicioSesion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public bool ValidarCredenciales()
+        {
+            string email_form = TB_email.Text.Trim();
+            string contraseña_form = TB_contraseña.Text;
+            bool credencialesValidas = false;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Persona WHERE Email = @Email AND Contraseña = @Contraseña";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Email", email_form);
+                cmd.Parameters.AddWithValue("@Contraseña", contraseña_form);
+
+                try
+                {
+                    conn.Open();
+                    int count = (int)cmd.ExecuteScalar();
+                    credencialesValidas = count > 0;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al validar credenciales: " + ex.Message);
+                }
+            }
+            return credencialesValidas;
+
+        }
+
+        private void TB_email_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void B_InicioSesion_Click(object sender, EventArgs e)
+        {
+            if (ValidarCredenciales())
+            {
+                MessageBox.Show("Bienvenido");
+                // Aquí puedes continuar con el siguiente formulario o lógica
+                contadorIntentos = 0; // Reiniciar en caso de éxito
+            }
+            else
+            {
+                contadorIntentos++;
+                if (contadorIntentos < 3)
+                {
+                    MessageBox.Show("Credenciales incorrectas. Intente nuevamente.");
+                    TB_email.Clear();
+                    TB_contraseña.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Ha superado el número de intentos. Espere 30 segundos para intentar nuevamente.");
+                    B_InicioSesion.Enabled = false;
+
+                    // Espera de 30 segundos usando un temporizador
+                    System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+                    timer.Interval = 30000; // 30 segundos
+                    timer.Tick += (s, args) =>
+                    {
+                        B_InicioSesion.Enabled = true;
+                        contadorIntentos = 0;
+                        timer.Stop();
+                        MessageBox.Show("Ya puede volver a intentar.");
+                    };
+                    timer.Start();
+                }
+            }
+        }
+    }
+}
